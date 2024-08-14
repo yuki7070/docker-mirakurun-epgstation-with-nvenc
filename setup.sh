e4a530d3a1249d52b75922a7f8de1896df145872
yuki tomori <tomori.y.ae@gmail.com>
2024-08-14 12:44:50 +0900

    セットアップをNVENC対応

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-git clone https://github.com/l3tnun/docker-mirakurun-epgstation.git
+git clone https://github.com/yuki7070/docker-mirakurun-epgstation-with-nvenc.git
 cd docker-mirakurun-epgstation
 cp docker-compose-sample.yml docker-compose.yml
 cp epgstation/config/enc.js.template epgstation/config/enc.js
+cp epgstation/config/av1_nvenc.sh.template epgstation/config/av1_nvenc.sh
+cp epgstation/config/h264_nvenc.sh.template epgstation/config/h264_nvenc.sh
+cp epgstation/config/h265_nvenc.sh.template epgstation/config/h265_nvenc.sh
 cp epgstation/config/config.yml.template epgstation/config/config.yml
 cp epgstation/config/operatorLogConfig.sample.yml epgstation/config/operatorLogConfig.yml
 cp epgstation/config/epgUpdaterLogConfig.sample.yml epgstation/config/epgUpdaterLogConfig.yml
